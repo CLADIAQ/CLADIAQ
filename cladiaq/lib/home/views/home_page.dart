5d0138f536e22cc7939eba0e820fa735c63196b3
taghawilfred <taghawilfred@gmail.com>
2024-09-08 14:47:15 +0100

    added App Bar

--- cladiaq/lib/home/views/home_page.dart
+++ cladiaq/lib/home/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cladiaq/commons/widgets/cq_app_bar.dart';
 import 'package:cladiaq/commons/widgets/cq_buttomn_nav_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +12,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Home Page"),
-          ],
+      appBar: CqAppBar(hasNotifications: true, title: "Hello, Serah!"),
+      body: const SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("Home Screen")],
+          ),
         ),
       ),
       bottomNavigationBar: CqButtomnNavBar(),
